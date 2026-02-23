676209ea59e159bb3996b1fa7f96b9402caba2c3
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-02-23 11:45:01 +0100

    Add ReroutingDisconnectCode enum for typed rerouting disconnect code IDs
    Replace plain integer list with typed IntEnum for reroutingDisconnectCodeIds
    in SipConfiguration, matching the existing pattern used by codecIds and other
    enum-backed fields.

--- src/test/java/com/didww/sdk/resource/VoiceInTrunkTest.java
+++ src/test/java/com/didww/sdk/resource/VoiceInTrunkTest.java
@@ -6,6 +6,7 @@ import com.didww.sdk.repository.ApiResponse;
 import com.didww.sdk.resource.configuration.PstnConfiguration;
 import com.didww.sdk.resource.enums.CliFormat;
 import com.didww.sdk.resource.enums.Codec;
+import com.didww.sdk.resource.enums.ReroutingDisconnectCode;
 import com.didww.sdk.resource.enums.RxDtmfFormat;
 import com.didww.sdk.resource.enums.SstRefreshMethod;
 import com.didww.sdk.resource.enums.MediaEncryptionMode;
@@ -16,6 +17,7 @@ import com.didww.sdk.resource.configuration.SipConfiguration;
 import com.didww.sdk.resource.configuration.TrunkConfiguration;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
@@ -95,6 +97,89 @@ class VoiceInTrunkTest extends BaseTest {
         assertThat(config.getMediaEncryptionMode()).isEqualTo(MediaEncryptionMode.DISABLED);
         assertThat(config.getStirShakenMode()).isEqualTo(StirShakenMode.DISABLED);
         assertThat(config.getAllowedRtpIps()).isNull();
+        assertThat(config.getReroutingDisconnectCodeIds()).isNull();
+    }
+
+    @Test
+    void testCreateSipTrunkWithReroutingDisconnectCodes() {
+        wireMock.stubFor(post(urlPathEqualTo("/v3/voice_in_trunks"))
+                .withRequestBody(equalToJson(loadFixture("voice_in_trunks/create_10_request.json"), true, false))
+                .willReturn(aResponse()
+                        .withStatus(201)
+                        .withHeader("Content-Type", "application/vnd.api+json")
+                        .withBody(loadFixture("voice_in_trunks/create_10.json"))));
+
+        SipConfiguration sipConfig = new SipConfiguration();
+        sipConfig.setUsername("username");
+        sipConfig.setHost("216.58.215.110");
+        sipConfig.setSstRefreshMethodId(SstRefreshMethod.INVITE);
+        sipConfig.setPort(5060);
+        sipConfig.setCodecIds(Arrays.asList(Codec.PCMU, Codec.PCMA, Codec.G729, Codec.G723, Codec.TELEPHONE_EVENT));
+        sipConfig.setReroutingDisconnectCodeIds(Arrays.asList(
+                ReroutingDisconnectCode.SIP_400_BAD_REQUEST,
+                ReroutingDisconnectCode.SIP_402_PAYMENT_REQUIRED,
+                ReroutingDisconnectCode.SIP_403_FORBIDDEN,
+                ReroutingDisconnectCode.SIP_404_NOT_FOUND,
+                ReroutingDisconnectCode.SIP_408_REQUEST_TIMEOUT,
+                ReroutingDisconnectCode.SIP_409_CONFLICT,
+                ReroutingDisconnectCode.SIP_410_GONE,
+                ReroutingDisconnectCode.SIP_412_CONDITIONAL_REQUEST_FAILED,
+                ReroutingDisconnectCode.SIP_413_REQUEST_ENTITY_TOO_LARGE,
+                ReroutingDisconnectCode.SIP_414_REQUEST_URI_TOO_LONG,
+                ReroutingDisconnectCode.SIP_415_UNSUPPORTED_MEDIA_TYPE,
+                ReroutingDisconnectCode.SIP_416_UNSUPPORTED_URI_SCHEME,
+                ReroutingDisconnectCode.SIP_417_UNKNOWN_RESOURCE_PRIORITY,
+                ReroutingDisconnectCode.SIP_420_BAD_EXTENSION,
+                ReroutingDisconnectCode.SIP_421_EXTENSION_REQUIRED,
+                ReroutingDisconnectCode.SIP_422_SESSION_INTERVAL_TOO_SMALL,
+                ReroutingDisconnectCode.SIP_423_INTERVAL_TOO_BRIEF,
+                ReroutingDisconnectCode.SIP_424_BAD_LOCATION_INFORMATION,
+                ReroutingDisconnectCode.SIP_428_USE_IDENTITY_HEADER,
+                ReroutingDisconnectCode.SIP_429_PROVIDE_REFERRER_IDENTITY,
+                ReroutingDisconnectCode.SIP_433_ANONYMITY_DISALLOWED,
+                ReroutingDisconnectCode.SIP_436_BAD_IDENTITY_INFO,
+                ReroutingDisconnectCode.SIP_437_UNSUPPORTED_CERTIFICATE,
+                ReroutingDisconnectCode.SIP_438_INVALID_IDENTITY_HEADER,
+                ReroutingDisconnectCode.SIP_480_TEMPORARILY_UNAVAILABLE,
+                ReroutingDisconnectCode.SIP_482_LOOP_DETECTED,
+                ReroutingDisconnectCode.SIP_483_TOO_MANY_HOPS,
+                ReroutingDisconnectCode.SIP_484_ADDRESS_INCOMPLETE,
+                ReroutingDisconnectCode.SIP_485_AMBIGUOUS,
+                ReroutingDisconnectCode.SIP_486_BUSY_HERE,
+                ReroutingDisconnectCode.SIP_487_REQUEST_TERMINATED,
+                ReroutingDisconnectCode.SIP_488_NOT_ACCEPTABLE_HERE,
+                ReroutingDisconnectCode.SIP_494_SECURITY_AGREEMENT_REQUIRED,
+                ReroutingDisconnectCode.SIP_500_SERVER_INTERNAL_ERROR,
+                ReroutingDisconnectCode.SIP_501_NOT_IMPLEMENTED,
+                ReroutingDisconnectCode.SIP_502_BAD_GATEWAY,
+                ReroutingDisconnectCode.SIP_504_SERVER_TIME_OUT,
+                ReroutingDisconnectCode.SIP_505_VERSION_NOT_SUPPORTED,
+                ReroutingDisconnectCode.SIP_513_MESSAGE_TOO_LARGE,
+                ReroutingDisconnectCode.SIP_580_PRECONDITION_FAILURE,
+                ReroutingDisconnectCode.SIP_600_BUSY_EVERYWHERE,
+                ReroutingDisconnectCode.SIP_603_DECLINE,
+                ReroutingDisconnectCode.SIP_604_DOES_NOT_EXIST_ANYWHERE,
+                ReroutingDisconnectCode.SIP_606_NOT_ACCEPTABLE,
+                ReroutingDisconnectCode.RINGING_TIMEOUT
+        ));
+        sipConfig.setMediaEncryptionMode(MediaEncryptionMode.ZRTP);
+        sipConfig.setStirShakenMode(StirShakenMode.PAI);
+        sipConfig.setAllowedRtpIps(Arrays.asList("127.0.0.1"));
+
+        VoiceInTrunk trunk = new VoiceInTrunk();
+        trunk.setName("hello, test sip trunk");
+        trunk.setConfiguration(sipConfig);
+
+        ApiResponse<VoiceInTrunk> response = client.voiceInTrunks().create(trunk);
+        VoiceInTrunk created = response.getData();
+        assertThat(created.getConfiguration()).isInstanceOf(SipConfiguration.class);
+
+        SipConfiguration config = (SipConfiguration) created.getConfiguration();
+        List<ReroutingDisconnectCode> codes = config.getReroutingDisconnectCodeIds();
+        assertThat(codes).hasSize(45);
+        assertThat(codes.get(0)).isEqualTo(ReroutingDisconnectCode.SIP_400_BAD_REQUEST);
+        assertThat(codes.get(codes.size() - 1)).isEqualTo(ReroutingDisconnectCode.RINGING_TIMEOUT);
+        assertThat(codes).contains(ReroutingDisconnectCode.SIP_480_TEMPORARILY_UNAVAILABLE);
     }
 
     @Test
