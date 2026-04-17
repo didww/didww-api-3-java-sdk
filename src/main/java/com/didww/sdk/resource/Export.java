e6f9d99d8f668a8791c95e13eaeb321a03ede9ec
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:39:49 +0200

    feat: surface STATUS_PENDING and STATUS_PROCESSING on Export

--- src/main/java/com/didww/sdk/resource/Export.java
+++ src/main/java/com/didww/sdk/resource/Export.java
@@ -3,6 +3,7 @@ package com.didww.sdk.resource;
 import com.didww.sdk.resource.enums.CallbackMethod;
 import com.didww.sdk.resource.enums.ExportStatus;
 import com.didww.sdk.resource.enums.ExportType;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.github.jasminb.jsonapi.annotations.Type;
 import lombok.Getter;
@@ -57,5 +58,20 @@ public class Export extends BaseResource {
 
     public void setExternalReferenceId(String externalReferenceId) {
         this.externalReferenceId = markDirty("externalReferenceId", externalReferenceId);
+    }
+
+    @JsonIgnore
+    public boolean isPending() {
+        return ExportStatus.PENDING.equals(status);
+    }
+
+    @JsonIgnore
+    public boolean isProcessing() {
+        return ExportStatus.PROCESSING.equals(status);
+    }
+
+    @JsonIgnore
+    public boolean isCompleted() {
+        return ExportStatus.COMPLETED.equals(status);
     }
 }
