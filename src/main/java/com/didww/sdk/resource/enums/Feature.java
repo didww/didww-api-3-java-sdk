2b6f4a53c0bd4543b09377f28797afc1a168b7ae
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:32:10 +0200

    feat: add p2p/a2p/emergency/cnam_out features to DidGroup for 2026-04-16

--- src/main/java/com/didww/sdk/resource/enums/Feature.java
+++ src/main/java/com/didww/sdk/resource/enums/Feature.java
@@ -6,5 +6,9 @@ public enum Feature {
     @JsonProperty("voice_in") VOICE_IN,
     @JsonProperty("voice_out") VOICE_OUT,
     @JsonProperty("t38") T38,
-    @JsonProperty("sms_in") SMS_IN;
+    @JsonProperty("sms_in") SMS_IN,
+    @JsonProperty("p2p") P2P,
+    @JsonProperty("a2p") A2P,
+    @JsonProperty("emergency") EMERGENCY,
+    @JsonProperty("cnam_out") CNAM_OUT;
 }
