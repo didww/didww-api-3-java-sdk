f35f664c2e65b1385ceba110a9394fd74ca146c6
Igor Fedoronchuk <fedoronchuk@gmail.com>
2026-04-17 10:36:01 +0200

    fix: declare has_one :address on EmergencyCallingService to mirror server

--- src/main/java/com/didww/sdk/resource/EmergencyCallingService.java
+++ src/main/java/com/didww/sdk/resource/EmergencyCallingService.java
@@ -52,6 +52,9 @@ public class EmergencyCallingService extends BaseResource {
     @Relationship("emergency_verification")
     private EmergencyVerification emergencyVerification;
 
+    @Relationship("address")
+    private Address address;
+
     @Relationship("dids")
     private List<Did> dids;
 }
